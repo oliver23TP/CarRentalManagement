5a7f1922e93bf1b7e8952839b03c67eb6b22304d
CHAY WEI HONG OLIVER 2401664D <2401664D@student.tp.edu.sg>
2025-11-24 16:39:11 +0800

    Added Identity

--- CarRentalManagement/Data/CarRentalManagementContext.cs
+++ CarRentalManagement/Data/CarRentalManagementContext.cs
@@ -1,36 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using CarRentalManagement.Components.Domain;
+﻿using CarRentalManagement.Components.Domain;
 using CarRentalManagement.Configurations.Entities;
-
+using CarRentalManagement.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 namespace CarRentalManagement.Data
 {
-    public class CarRentalManagementContext : DbContext
+    public class CarRentalManagementContext(DbContextOptions<CarRentalManagementContext> options) :
+   IdentityDbContext<CarRentalManagementUser>(options)
     {
-        public CarRentalManagementContext (DbContextOptions<CarRentalManagementContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<CarRentalManagement.Components.Domain.Make> Make { get; set; } = default!;
-        public DbSet<CarRentalManagement.Components.Domain.Model> Model { get; set; } = default!;
-        public DbSet<CarRentalManagement.Components.Domain.Colour> Colour { get; set; } = default!;
-        public DbSet<CarRentalManagement.Components.Domain.Customer> Customer { get; set; } = default!;
-        public DbSet<CarRentalManagement.Components.Domain.Vehicle> Vehicle { get; set; } = default!;
-        public DbSet<CarRentalManagement.Components.Domain.Booking> Booking { get; set; } = default!;
-
+        public DbSet<Make> Make { get; set; } = default!;
+        public DbSet<Model> Model { get; set; } = default!;
+        public DbSet<Colour> Colour { get; set; } = default!;
+        public DbSet<Vehicle> Vehicle { get; set; } = default!;
+        public DbSet<Booking> Booking { get; set; } = default!;
+        public DbSet<Customer> Customer { get; set; } = default!;
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             builder.ApplyConfiguration(new ColourSeed());
             builder.ApplyConfiguration(new MakeSeed());
             builder.ApplyConfiguration(new ModelSeed());
-
         }
     }
-
-
 }
